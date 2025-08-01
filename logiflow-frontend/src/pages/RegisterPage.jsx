35c9324f95febaa2062c7a879cc07981b5cdf19d
ozziron <ozdemirumut@yahoo.com>
2025-08-01 15:29:19 +0300

    Update RegisterPage.jsx

--- logiflow-frontend/src/pages/RegisterPage.jsx
+++ logiflow-frontend/src/pages/RegisterPage.jsx
@@ -5,7 +5,7 @@ import { Button } from '../components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
 import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
-import { useToast } from '../hooks/use-toast'
+import { toast } from '../hooks/use-toast'
 import { UserPlus } from 'lucide-react'
 
 const RegisterPage = () => {
