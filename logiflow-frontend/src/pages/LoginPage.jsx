8995cac590097685fffd104e4389e73c614c890a
ozziron <ozdemirumut@yahoo.com>
2025-08-01 15:35:04 +0300

    Update LoginPage.jsx

--- logiflow-frontend/src/pages/LoginPage.jsx
+++ logiflow-frontend/src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { Button } from '../components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
 import { Input } from '../components/ui/input'
 import { Label } from '../components/ui/label'
-import useToast from '../hooks/use-toast.js'
+import { useToast } from '../hooks/use-toast.js'
 import { LogIn } from 'lucide-react'
 
 const LoginPage = () => {
