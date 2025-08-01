a5519a1b58dbf8b962873246630c7b3937d44560
ozziron <ozdemirumut@yahoo.com>
2025-08-01 15:34:35 +0300

    Update use-toast.js

--- logiflow-frontend/src/hooks/use-toast.js
+++ logiflow-frontend/src/hooks/use-toast.js
@@ -1,7 +1,13 @@
+// src/hooks/use-toast.js
 import { toast } from 'sonner';
 
+// Named export
 export const useToast = () => {
   return toast;
 };
 
+// Default export da ekleyelim
+export default useToast;
+
+// Ayrıca toast'u da export et (gerekirse)
 export { toast };
